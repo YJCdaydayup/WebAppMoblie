0730a0d9ec23cdcd75ca2a6003201e796c615208
杨力 <yangli@yanglideMacBook-Air.local>
2018-09-01 23:04:54 +0800

    添加轮播图

--- wxshop_mzxy/static/script/views/enterjs/mainconfig.js
+++ wxshop_mzxy/static/script/views/enterjs/mainconfig.js
@@ -1,17 +1,18 @@
-/**
- * Created by yangli on 2018/9/1.
- */
 require.config({
-   paths: {
-       jquery: "../../libs/jquery",
-       swiper: "../../libs/swiper.min"
-   }
+	paths:{
+		jquery:"../../libs/jquery",
+		swiper:"../../plugs/swiper.min"
+
+	}
 });
 
-require(['jquery','swiper'],function () {
-   var topSlider = new Swiper('#topSlider',{
-       slidesPerView:1,
-       centeredSlides: true
-   });
-
+// swiper依赖jquery，需要引进jquery
+require(['jquery','swiper'],function(){
+	var topSlider=new Swiper('#topSlider', {
+        slidesPerView: 1, // 一下滑一个
+        centeredSlides: true, // 滑动到中间
+        autoplay: 3000,
+        loop: true,
+        autoplayDisableOnInteraction: true // 手动滑动时禁止自动滑动
+    });
 });
